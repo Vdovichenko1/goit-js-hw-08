347d6c3c0ab66d74ab1f4b4d034d9896b09d191f
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-08-22 14:01:17 +0300

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
 
+const REFUGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
