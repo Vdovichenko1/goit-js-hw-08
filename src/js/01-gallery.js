e1fee42341e3932e51076942866bd2075f8bae30
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-08-22 13:11:05 +0300

    1+3 work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItems(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+            </a>
+            </div>`;
+    })
+    .join('');
+}
+
+const galItem = createGalleryItems(galleryItems);
+
+galleryContainer.innerHTML = galItem;
+
+galleryContainer.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const REFUGE_KEY = 'feedback-form-state';
+
+const feedbackForm = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  mail: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+refs.form.addEventListener('input', e => {
+  feedbackForm[e.target.name] = e.target.value;
+  localStorage.setItem(REFUGE_KEY, JSON.stringify(feedbackForm));
+});
+
+messageOutput();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(REFUGE_KEY);
+}
+
+function onTextareaInput(e) {
+  const message = e.target.value;
+
+  localStorage.setItem(REFUGE_KEY, message);
+}
+
+function messageOutput() {
+  const savedMessage = JSON.parse(localStorage.getItem(REFUGE_KEY));
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+    refs.mail.value = data.mail;
+  }
+}
