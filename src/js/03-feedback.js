7b021565a29be88c0249266363c30e48b475efda
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-08-23 16:30:38 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,48 @@
 import throttle from 'lodash.throttle';
 
 const REFUGE_KEY = 'feedback-form-state';
-
-const feedbackForm = {
-  email: '',
-  textarea: '',
-};
-
+let savedMessage = null;
 const refs = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
-refs.form.addEventListener('input', e => {
-  feedbackForm[e.target.name] = e.target.value;
-  localStorage.setItem(REFUGE_KEY, JSON.stringify(feedbackForm));
-});
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 messageOutput();
+
+const feedbackForm = {
+  email: savedMessage?.email ? savedMessage.email : '',
+  message: savedMessage?.message ? savedMessage.message : '',
+};
+
+function onFormInput(e) {
+  feedbackForm[e.target.name] = e.target.value;
+  localStorage.setItem(REFUGE_KEY, JSON.stringify(feedbackForm));
+}
 
 function onFormSubmit(e) {
   e.preventDefault();
 
   e.currentTarget.reset();
   localStorage.removeItem(REFUGE_KEY);
-}
-
-function onTextareaInput(e) {
-  const message = e.target.value;
-
-  localStorage.setItem(REFUGE_KEY, message);
+  console.log(feedbackForm);
+  feedbackForm.email = '';
+  feedbackForm.message = '';
 }
 
 function messageOutput() {
-  const savedMessage = JSON.parse(localStorage.getItem(REFUGE_KEY));
+  savedMessage = JSON.parse(localStorage.getItem(REFUGE_KEY));
 
   if (savedMessage) {
-    refs.textarea.value = savedMessage.textarea;
-    refs.email.value = savedMessage.email;
+    if (savedMessage.email) {
+      refs.email.value = savedMessage.email;
+    }
+    if (savedMessage.message) {
+      refs.textarea.value = savedMessage.message;
+    }
   }
 }
